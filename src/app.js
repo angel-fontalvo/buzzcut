0980b69a8b0da8cb4b82cf21fd8c6c1490111849
Angel Fontalvo <afontalv@outlook.com>
2019-11-17 16:49:13 -0500

    prepping for deployingment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
@@ -12,7 +11,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(compression()); //Compress all routes
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'dist')));
